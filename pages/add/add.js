b8c8356fb5902e9f1c21f2982c0638bba61fc768
Niels van Klooster <nielsvanklooster@gmail.com>
2018-08-07 17:47:57 +0800

    created to add page, added a button for taking pictures + js. Field for description also done, but post picture button not yet linked with js

--- pages/add/add.js
+++ pages/add/add.js
@@ -1,8 +1,22 @@
 // pages/add/add.js
 const app = getApp()
+const myRequest = require('../../lib/request');
 
 Page({
+  picture: function(e) {
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'], 
+      sourceType: ['album', 'camera'], 
+      success: function (res) {
+        var tempFilePaths = res.tempFilePaths
+      },
+    })
+  },
 
-
-
+  goToIndex: function () {
+    wx.reLaunch({
+      url: '../index/index'
+    })
+  },
 })
