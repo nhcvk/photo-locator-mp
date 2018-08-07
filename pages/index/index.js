3cb2c7b67c019fb914f62a8c839e3afb34908774
Sven den Boer <denboer.sven@gmail.com>
2018-08-07 15:06:48 +0800

    added iteration

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,22 @@
 //index.js
 const app = getApp()
+const myRequest = require('../../lib/request');
 
 Page({
+
+    data: {
+        items: []
+      },
+
+      onLoad: function () {
+        let page = this
+        myRequest.get({
+          path: 'photos',
+          success(res) {
+            console.log(res)
+            page.setData({items: res.data.photos}) 
+          }
+        })
+      },
 
 })
